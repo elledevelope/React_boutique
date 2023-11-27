aa71edaf9d7471d1019b2a7b3b88306ba44436f9
Elmira PETIT <elledevelope@gmail.com>
2023-11-27 12:13:23 +0100

    Created menuentries

---
+++ src/menuentries.js
@@ -0,0 +1,24 @@
+const menuentries = [
+    {
+        entry: "Home",
+        url: ""
+    },
+    {
+        entry: "Men's",
+        url: ""
+    },
+    {
+        entry: "Women's",
+        url: ""
+    },
+    {
+        entry: "Contact",
+        url: ""
+    },
+    // {
+    //     entry: "Panier ",
+    //     url: ""
+    // }
+];
+
+export { menuentries };
