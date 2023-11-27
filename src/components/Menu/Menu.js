48801994ec080d4536b6aa2a3ad13b9627b3da3d
Elmira PETIT <elledevelope@gmail.com>
2023-11-27 12:13:48 +0100

    Update Menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import './Menu.css';
 import Panier from '../Panier/Panier';
+import ListMenu from '../ListMenu/ListMenu';
 
 const Menu = (props) => {
   return (
     <div className="Menu">
-      <ul>
+
+      <nav id="menu">
+        <ListMenu
+          sendEntries={props.sendEntries}
+          handleDisplayPanier={props.handleDisplayPanier}></ListMenu>
+      </nav>
+
+      {/* <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">Men's</a></li>
         <li><a href="#">Women's</a></li>
         <li><a href="#">Contact</a></li>
-      </ul>
+      </ul> */} {/* FOAD 24.11.23 */}
 
       <div id="icon-panier" onClick={props.handelDisplayPanier}>
         <img src="./assets/img/cart-shopping-solid.svg" alt="icon-shopping-cart" />
       </div>
 
-      {props.displayPanier && <Panier handelDisplayPanier={props.handelDisplayPanier}></Panier>}
+      {/* {props.displayPanier && <Panier handelDisplayPanier={props.handelDisplayPanier}></Panier>} */} {/* FOAD 24.11.23 */}
 
     </div>
   );
