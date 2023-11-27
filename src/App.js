a2274106ba95eca6a8f3dc02969a3d7b12a04a3d
Elmira PETIT <elledevelope@gmail.com>
2023-11-27 12:14:56 +0100

    Update APP.js for Panier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Gallery from './components/Gallery/Gallery.js';
 import { articles } from './articles.js';
+import { menuentries } from "./menuentries.js";
 import './App.css';
 import Menu from "./components/Menu/Menu.js";
 import Panier from "./components/Panier/Panier.js";
+import BoutiqueContext from "./contexts/BoutiqueContext.js";
+
+
 
 
 const App = () => {
@@ -11,22 +15,11 @@ const App = () => {
   const [state, setState] = React.useState(
     {
       'articles': articles,
+      'achat': [],
     }
   );
 
-  //onClick <button> (Card), decrement number(qte) of articles in stock and do not go less than 0:
-  const decrementQte = (id) => {
-    //state.articles[id].qte--;
-    //option1 :
-    if (state.articles[id].qte > 0) {
-      let articlesTmp = state.articles;
-      articlesTmp[id].qte--;
-      setState({
-        'articles': articlesTmp,
-      })
-    }
-  }
-
+  //-------------------PANIER:
   const [statePanier, setStatePanier] = React.useState(
     {
       'displayPanier': false,
@@ -40,18 +33,53 @@ const App = () => {
   }
 
 
-  return (
-    <>
-      <header>
-        <Menu handelDisplayPanier={handelDisplayPanier}> </Menu>
-        {statePanier.displayPanier && <Panier ></Panier>}
+  //onClick <button> (Card), decrement number(qte) of articles in stock and do not go less than 0:
+  const decrementQte = (id) => {
 
+    //state.articles[id].qte--;
+    //option1 :
+    if (state.articles[id].qte > 0) {
+      let articlesTmp = state.articles;
+      articlesTmp[id].qte--;
+      setState({
+        'articles': articlesTmp,
+        //spread operator, //option2 :
+        'achat': [...state.achat, id]
+      })
+    }
+
+
+
+  };
+
+
+
+  return (
+    <BoutiqueContext.Provider value={state}>
+      <header>
+        <Menu
+          handelDisplayPanier={handelDisplayPanier}
+          sendEntries={menuentries}
+        > </Menu>
+
+        {/* Panier: */}
+        {/* {statePanier.displayPanier && <Panier ></Panier>}  */} {/* FOAD 24.11.23 */}
+        {statePanier.displayPanier ? <Panier
+          handelDisplayPanier={handelDisplayPanier}
+          achat={state.achat}
+        ></Panier> : <></>}
       </header>
+
+
       <main>
+        <div id="logo">
+          {/* <img src="./assets/img/logo-boutique.png" /> */}
+        </div>
+
         <Gallery articles={state.articles} decrementQte={decrementQte}></Gallery>
       </main>
       <footer></footer>
-    </>
+    </BoutiqueContext.Provider>
   );
 }
 
