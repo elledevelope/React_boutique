f059323a8eecd7e176d75dc03d58a6579c21fded
Elmira PETIT <elledevelope@gmail.com>
2023-11-27 12:15:19 +0100

    Created context: Achat, BoutiqueContext

---
+++ src/components/Achat/Achat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Achat.css';
+
+const Achat = (props) => (
+  <div className="Achat">
+    <div> Item X</div>
+  </div>
+);
+
+
+export default Achat;
---
+++ src/contexts/BoutiqueContext.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const BoutiqueContext = React.createContext();
+
+export default BoutiqueContext;
