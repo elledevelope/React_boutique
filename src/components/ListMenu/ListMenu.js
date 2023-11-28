0edbbb7838e4ad68fa3b8787412e66d3d0c8a2a7
Elmira PETIT <elledevelope@gmail.com>
2023-11-28 13:09:43 +0100

    Update component ListMenu

--- src/components/ListMenu/ListMenu.js
+++ src/components/ListMenu/ListMenu.js
@@ -4,21 +4,31 @@ import Panier from '../Panier/Panier';
 
 const ListMenu = (props) => {
   return (
-    <ul className="ListMenu">
-      {
-        props.sendEntries.map((value, index) => {
-          
-          return (
-              <li 
-                /* un event ne peut pas être directement conditionné */
-                onClick={
-                 value.entry === "Panier" ? props.handleDisplayPanier : null
-                }
-              key={index}>{value.entry}</li>  
-          )
-        })
-      }
-    </ul>
+    <>
+      <div className="ListMenu nav-links">
+        <div className="close-icon">
+          <img src="./assets/img/icons/x.svg" />
+        </div>
+
+        <ul >
+          {
+            props.sendEntries.map((value, index) => {
+
+              return (
+                <li
+                  /* un event ne peut pas être directement conditionné */
+                  onClick={
+                    value.entry === "Panier" ? props.handleDisplayPanier : null
+                  }
+                  key={index}>{value.entry}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+
+      
+    </>
   )
 };
 
