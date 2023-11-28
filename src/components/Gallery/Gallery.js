3514e840c8eac2ea03c19a66a5a55e1454f207a5
Elmira PETIT <elledevelope@gmail.com>
2023-11-28 13:09:55 +0100

    Update Gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,14 +3,19 @@ import './Gallery.css';
 import Card from '../Card/Card';
 
 
-const Gallery = (props) => (
-  <div className="Gallery">
-    {
-      props.articles.map((value, index) => (
-        <Card key={index} article={value} decrementQte={props.decrementQte}></Card>
-      ))
-    }
-  </div>
-);
+const Gallery = (props) => {
+  return (
+    <>
+      <div className="Gallery">
+        {
+          props.articles.map((value, index) => (
+            <Card key={index} article={value} decrementQte={props.decrementQte}></Card>
+          ))
+        }
+      </div>
+      <div className='btnShow'>Show more </div>
+    </>
+  )
+};
 
 export default Gallery;
