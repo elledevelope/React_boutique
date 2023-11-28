7af631367eb366f4a1a7c0f1faa2c42837f744b3
Elmira PETIT <elledevelope@gmail.com>
2023-11-28 13:08:56 +0100

    Created component Achat

--- src/components/Achat/Achat.js
+++ src/components/Achat/Achat.js
@@ -1,12 +1,35 @@
 import React from 'react';
-
 import './Achat.css';
+import BoutiqueContext from "../../contexts/BoutiqueContext";
 
-const Achat = (props) => (
-  <div className="Achat">
-    <div> Item X</div>
-  </div>
-);
+const Achat = (props) => {
+  let id = props.item.idachat;
+  const boutiqueContext = React.useContext(BoutiqueContext);
+  let article = boutiqueContext.articles[id];
+
+  return (
+    <div className="Achat">
+      <div className='achatImg'>
+        <img src={"./assets/img/" + article.img} alt={article.name} />
+      </div>
+
+
+      <div> {article.name} </div>
+
+      <div id="sub_achat">
+        <div className="achatQte">
+          <span className="AchatBtn btnPlus">-</span>
+          <span className='achatQte'> {props.item.qteachat} </span>
+          <span className="AchatBtn btnLess" onClick={
+            () => boutiqueContext.decrementQte(id)}
+          >+</span>
+        </div>
+        <span className='achatPrix'> {article.prix} €  </span>
+      </div>
+
+    </div>
+  )
+};
 
 
 export default Achat;
