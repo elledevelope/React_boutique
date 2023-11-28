fd83da0ca5cb0303a19c9683df8cfb85f2bdd3e3
Elmira PETIT <elledevelope@gmail.com>
2023-11-28 13:10:05 +0100

    Update Panier

--- src/components/Panier/Panier.js
+++ src/components/Panier/Panier.js
@@ -11,7 +11,7 @@ const Panier = (props) => (
 
         {
           props.achat.map(
-            (value, index) => <Achat id={value} key={index}></Achat>)
+            (value, index) => <Achat item={value} key={index}></Achat>)
         }
 
 
